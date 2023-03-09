e413ce9419934657d045e5411fd4606f17c0f7a9
WintrCat <wintrcat.wilson@gmail.com>
2023-03-09 00:09:29 +0000

    Remove build script

--- build.ps1
+++
@@ -1,7 +0,0 @@
-$time = Get-Date
-rm -r dist/*
-tsc
-cp src/* -d dist -r -EA Ignore
-rm -r dist/public/**/*.ts
-$elapsed = [math]::round(($(Get-Date) - $time).TotalSeconds, 3)
-& echo "Compiled successfully. ($elapsed seconds)"
