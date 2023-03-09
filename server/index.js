9fa0cddd7011e5af69731a36bd59376ca3aa3714
WintrCat <wintrcat.wilson@gmail.com>
2023-03-09 00:27:27 +0000

    TS stuff

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
-const express = require("express");
-
-let server = express();
-
-server.use("/", express.static("../client/main"));
-
-server.listen(8080, () => {
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var server = (0, express_1["default"])();
+server.use("/", express_1["default"].static("../client/main"));
+server.listen(8080, function () {
     console.log("Running!");
 });
